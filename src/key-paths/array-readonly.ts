2bfd70530920f534525c2a7f2d43af757e6b4583
Toma91 <andrea.tomarelli@gmail.com>
2022-02-28 14:55:03 +0100

    Create array-readonly.ts

---
+++ src/key-paths/array-readonly.ts
@@ -0,0 +1,30 @@
+type ArrayKeyPathForNullableArrayValue<Root extends object, NonNullableArrayValue> = NonNullableArrayValue extends any[] ? {
+  [Index in IndexesOf<NonNullableArrayValue>]: KeyPath<Root, NonNullableArrayValue[Index] | null>
+} & {
+  length: KeyPath<Root, number | null>
+  [get]: KeyPathGetter<Root, NonNullableArrayValue | null>
+} : never
+
+type ArrayKeyPathForUndefineableArrayValue<Root extends object, NonUndefineableArrayValue> = NonUndefineableArrayValue extends any[] ? {
+  [Index in IndexesOf<NonUndefineableArrayValue>]: KeyPath<Root, NonUndefineableArrayValue[Index] | undefined>
+} & {
+  length: KeyPath<Root, number | undefined>
+  [get]: KeyPathGetter<Root, NonUndefineableArrayValue | undefined>
+} : never
+
+type ArrayKeyPathForRealArrayValue<Root extends object, ArrayValue> = ArrayValue extends any[] ? {
+  [Index in IndexesOf<ArrayValue>]: KeyPath<Root, ArrayValue[Index]>
+} & {
+  length: KeyPath<Root, number>
+  [get]: KeyPathGetter<Root, ArrayValue>    
+} : never
+
+type ArrayKeyPath<Root extends object, ArrayValue> = IfContainsNull<
+  ArrayValue,
+  ArrayKeyPathForNullableArrayValue<Root, Exclude<ArrayValue, null>>,
+  IfContainsUndefined<
+    ArrayValue,
+    ArrayKeyPathForUndefineableArrayValue<Root, Exclude<ArrayValue, undefined>>,
+    ArrayKeyPathForRealArrayValue<Root, ArrayValue>
+  >
+>
